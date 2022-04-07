9d22d9dcb6e998dec6da2c4f0afef67b6d46cbfd
ManbirK <72926520+ManbirK@users.noreply.github.com>
2022-04-07 17:51:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,38 +1,135 @@
-Canvas = document.getElementById("myCanvas");
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
 
-ctx = Canvas.getContext('2d');
+//Define the width & height of the rover image.
+rover_width = 110;
+rover_height = 77;
 
 
-rover_img = "rover.png";
-background_img = "mars.jpg";
+background_image = "mars.jpg";
 
-rover_x = 10;
-rover_y = 10;
+rover_image = "rover.png";
 
-rover_width = 100; 
-rover_height = 90;
+rover_x = 200;
+rover_y = 300;
 
-function add(){
+//Create "add()" function
+ function add(){
+     background_Tag = new Image(); //defining a variable with a new image
+     background_Tag.onload = uploadBackground; // setting a function, onloading this variable
+     background_Tag.src = background_image; // load image
 
-    background_Tag = new Image();
-    background_Tag.onload = upload_background;
-    background_Tag.src = background_img;
 
-    rover_tag = new Image();
-    rover_tag.onload = upload_rover;
-    rover_tag.src = rover_img;
+
+     rover_Tag = new Image();  //defining a variable with a new image
+     rover_Tag.onload = uploadRover;  // setting a function, onloading this variable
+     rover_Tag.src = rover_image;   // load image
+
+
+ }
+	                                     
+	                                     
+	                                     
+
+                                         
+	                                    
+	                                   
+//Create "uploadBackground()" function.
+ function uploadBackground(){
+     //Draw image of background
+     ctx.drawImage(background_Tag, 0, 0, canvas.width, canvas.height); // 2dContext of canvas.drawImage(frame,x,y,Width,Height);
+ }   
+ 
+ //Create "uploadrover()" function.
+ function uploadRover(){
+     
+    //Draw image of rover
+
+     ctx.drawImage(rover_Tag, rover_x, rover_y, rover_width, rover_height);
+ }                          
+                                         
+
+
+
+
+window.addEventListener("keydown", my_keydown);
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+    //Write code if keypressed is up. ASCII value of "up" arrow is 38.
+		if (keyPressed == "38"){
+            console.log(" Up arrow pressed");
+            up();
+        }
     
+
+
+     //Write code if keypressed is down. ASCII value of "up" arrow is 40.
+     if (keyPressed == "40"){
+         console.log(" Down arrow pressed");
+         down();
+     }
+
+      //Write code if keypressed is left. ASCII value of "left" arrow is 37.
+     if (keyPressed == "37"){
+        console.log(" Left arrow pressed");
+        left();
+     }
+
+      //Write code if keypressed is right. ASCII value of "right" arrow is 39.
+     if (keyPressed == "39"){
+         console.log(" Right arrow pressed");
+         right();
+     }
+
+    
+		
+
+
+
+
+    //Additional Activity
+    //Write the code for left and right arrow pressed. 
 }
 
 
-function upload_background(){
-
-    ctx.drawImage(background_Tag,0,0,Canvas.width, Canvas.height);
-
+function up(){
+        if (rover_y > 10){
+        rover_y -= 10; // rover_y = rover_y -10;
+        console.log(rover_y);
+        uploadBackground();
+        uploadRover();
+        }
 }
 
-function upload_rover(){
-
-    ctx.drawImage(rover_tag, rover_x, rover_y, rover_width, rover_height)
-
+function down(){
+	if (rover_y < 513){
+        rover_y += 10;
+        console.log(rover_y);
+        uploadBackground();
+        uploadRover();
+    }
 }
+
+function right(){
+	
+    if (rover_x < 680){
+        rover_x += 10;
+        console.log(rover_x);
+        uploadBackground();
+        uploadRover();
+    }
+}
+
+function left(){
+	if (rover_x > 10){
+        rover_x -= 10;
+        console.log(rover_x);
+        uploadBackground();
+        uploadRover();
+    }
+}
+
+
+
